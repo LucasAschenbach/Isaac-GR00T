1d9d949a9ee8f6539ffb4f56de6489b259349c01
LucasAschenbach <lucas.aschenbach@gmail.com>
2025-06-14 21:52:53 +0000

    Update inference server for gr00t tairo

--- scripts/inference_service.py
+++ scripts/inference_service.py
@@ -111,14 +111,21 @@ def main(args: ArgsConfig):
         # - action: action.left_hand: (16, 6)
         # - action: action.right_hand: (16, 6)
         # - action: action.waist: (16, 3)
+        # obs = {
+        #     "video.ego_view": np.random.randint(0, 256, (1, 256, 256, 3), dtype=np.uint8),
+        #     "state.left_arm": np.random.rand(1, 7),
+        #     "state.right_arm": np.random.rand(1, 7),
+        #     "state.left_hand": np.random.rand(1, 6),
+        #     "state.right_hand": np.random.rand(1, 6),
+        #     "state.waist": np.random.rand(1, 3),
+        #     "annotation.human.action.task_description": ["do your thing!"],
+        # }
         obs = {
-            "video.ego_view": np.random.randint(0, 256, (1, 256, 256, 3), dtype=np.uint8),
-            "state.left_arm": np.random.rand(1, 7),
-            "state.right_arm": np.random.rand(1, 7),
-            "state.left_hand": np.random.rand(1, 6),
-            "state.right_hand": np.random.rand(1, 6),
-            "state.waist": np.random.rand(1, 3),
-            "annotation.human.action.task_description": ["do your thing!"],
+            "video.front": np.random.randint(0, 256, (1, 480, 640, 3), dtype=np.uint8),
+            "video.wrist": np.random.randint(0, 256, (1, 480, 640, 3), dtype=np.uint8),
+            "state.single_arm": np.random.rand(1, 5),
+            "state.gripper": np.random.rand(1, 1),
+            "annotation.human.action.task_description": ["Pick the pill out of the MORNING slot of the pill box"],
         }
 
         time_start = time.time()
